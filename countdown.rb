ed49f8d6f13a54e519c1bfb3e53c5b24366d8178
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-07 19:13:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
+#puts "Enter countdown time: "
+#time_to_go = gets.chomp
+#number = time_to_go
 
-def countdown
+def countdown(time_to_go)
+  while time_to_go > 0
+    puts "#{time_to_go} SECOND(S)!"
+    time_to_go-=1
+  end
+  puts "HAPPY NEW YEAR!"
+  
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number-=1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+puts "Enter countdown time: "
+time_to_go = gets.chomp
+number = time_to_go
+countdown(time_to_go)
+countdown_with_sleep(number)
+
