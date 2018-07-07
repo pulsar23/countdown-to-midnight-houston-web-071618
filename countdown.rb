cb55ebacb7f45b7e0ccab5d9cd5f7041020b0dfb
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-07 19:26:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
 
-def countdown
+
+def countdown(time_to_go)
+  while time_to_go > 0
+    puts "#{time_to_go} SECOND(S)!"
+    time_to_go-=1
+  end
+  return "HAPPY NEW YEAR!"
+  
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number-=1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+
